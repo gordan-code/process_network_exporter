1d7cf5beed0b6208b4f89d88bf8243e7f7ee175e
gordan <gordanzhang@163.com>
2020-09-03 14:34:38 +0800

    已添加日志功能

---
+++ util/ds.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"reflect"
+)
+
+//go:generate msgp
+
+type Config struct {
+	Check_interval_seconds  float64   `yaml:"check_interval_seconds"`
+	Http_server_port  string  `yaml:"http_server_port"`
+	Check_processes  []string  `yaml:"check_processes"`
+	Excluded_users []string `yaml:"excluded_users"`
+}
+//type DataKey struct {
+//	Pid     string		`json:"pid"`
+//	Src     string		`json:"src"`
+//	Dst     string		`json:"dst"`
+//	TypeStr string		`json:"type"`
+//}
+type DataValue struct {
+	User		string
+	Name        string  //cmd of process
+	Status      string
+	Create_time string
+	End_time    string
+}
+func (x DataValue) IsStructureEmpty() bool {
+	return reflect.DeepEqual(x, DataValue{})
+}
+type TCPInfo struct{
+	Laddr  string
+	Raddr  string
+	Status string
+}
+type Process struct{
+	User string
+	Pid  string
+	Cmd  string
+}
